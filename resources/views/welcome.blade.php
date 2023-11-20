ad21b42726ad9d5c85297242ea12d1ba71ddf181
arislamp <129835271+arislamp@users.noreply.github.com>
2023-11-20 20:27:05 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Other head content -->
+</head>
+<body>
+    <div id="app"></div>
+    <script src="{{ mix('/js/app.js') }}"></script>
+</body>
+</html>
