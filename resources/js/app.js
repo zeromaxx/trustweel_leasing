357c6864ee64d0a6582799c1e20157fac86c63e8
Αριστείδης Λαμπρινίδης <91885928+zeromaxx@users.noreply.github.com>
2023-11-30 17:24:51 +0200

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import Home from "./components/Home";
+import Login from "./components/Login";
 import NotFoundPage from './components/NotFoundPage'
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Home/>} />
+            <Route path="/login" element={<Login/>} />
             <Route path="*" element={<NotFoundPage />} />
         </Routes>
     );
