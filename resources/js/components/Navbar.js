29621ea5b0c72f3aa755df4fca3af30ddb27c35f
Αριστείδης Λαμπρινίδης <91885928+zeromaxx@users.noreply.github.com>
2023-11-30 17:25:14 +0200

    Update Navbar.js

--- resources/js/components/Navbar.js
+++ resources/js/components/Navbar.js
@@ -5,22 +5,30 @@ import LogoImage from "../../../public/img/LPALDlogoheader.webp";
 function NavBar() {
     return (
         <div className="header">
-            <div className="logo">
-                <img src={LogoImage} alt="" />
-            </div>
-
-            <ul className="nav-menu">
-                <Link to="/">Home</Link>
-                <Link to="/about">Fleet</Link>
-            </ul>
-
-            <div className="search-and-user">
-                <input type="search" placeholder="Search" />
-                <a href="#">Service & Contact</a>
-                <a href="#">My account</a>
-                <select name="languages" id="language-select">
-                    <option value="en">EN</option>
-                </select>
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                        <div className="logo">
+                            <img src={LogoImage} alt="" />
+                        </div>
+                    </div>
+                    <div className="col-4">
+                        <ul className="nav-menu">
+                            <Link to="/">Home</Link>
+                            <Link to="/about">Fleet</Link>
+                        </ul>
+                    </div>
+                    <div className="col-4">
+                        <div className="search-and-user">
+                            <input type="search" placeholder="Search" />
+                            <a href="#">Service & Contact</a>
+                            <Link to="/login">Login</Link>
+                            <select name="languages" id="language-select">
+                                <option value="en">EN</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
