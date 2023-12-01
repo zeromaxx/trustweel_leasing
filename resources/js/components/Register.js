13537f41ce7eca33080ec085da04144179b9f058
Αριστείδης Λαμπρινίδης <91885928+zeromaxx@users.noreply.github.com>
2023-12-01 16:35:42 +0200

    Update Register.js

--- resources/js/components/Register.js
+++ resources/js/components/Register.js
@@ -1,1 +1,25 @@
+import LogoImage from "../../../public/img/LPALDlogoheader.webp";
+export default function Register() {
+    return (
+        <section>
+            <div className="col-3">
+                <div className="login-wrapper p-3">
+                    <div className="text-center">
+                        <img src={LogoImage} alt="Logo" className="mb-4 w-100" />
+                        <h2 className="mb-4">Register Account</h2>
+                        <form>
+                            <div className="form-group mb-3">
+                                <input type="text" className="form-control" placeholder="Your Username" />
+                            </div>
+                            <div className="form-group mb-3">
+                                <input type="password" className="form-control" placeholder="Your Password" />
+                            </div>
+                            <button type="submit" className="btn btn-default">Register</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
 
