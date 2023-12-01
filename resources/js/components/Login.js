ea0b81cf36255981c3161b399340339e57da5fac
Αριστείδης Λαμπρινίδης <91885928+zeromaxx@users.noreply.github.com>
2023-12-01 11:23:28 +0200

    Update Login.js

--- resources/js/components/Login.js
+++ resources/js/components/Login.js
@@ -1,23 +1,25 @@
+import LogoImage from "../../../public/img/LPALDlogoheader.webp";
 export default function Login() {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-5">
-                    <div className="login-wrapper p-3">
-                        <div className="text-center">
-                            <img src="path_to_logo/logo.png" alt="Logo" className="mb-4" />
-                            <h2 className="mb-4">LeasePlan Identity</h2>
-                            <form>
-                                <div className="form-group mb-3">
-                                    <input type="text" className="form-control" placeholder="Your Username" />
-                                </div>
-                                <button type="submit" className="btn btn-default">Continue</button>
-                            </form>
-                        </div>
+        <section>
+            <div className="col-3">
+                <div className="login-wrapper p-3">
+                    <div className="text-center">
+                        <img src={LogoImage} alt="Logo" className="mb-4 w-100" />
+                        <h2 className="mb-4">Login</h2>
+                        <form>
+                            <div className="form-group mb-3">
+                                <input type="text" className="form-control" placeholder="Your Username" />
+                            </div>
+                            <div className="form-group mb-3">
+                                <input type="password" className="form-control" placeholder="Your Password" />
+                            </div>
+                            <button type="submit" className="btn btn-default">Login</button>
+                        </form>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
 
     )
 }
