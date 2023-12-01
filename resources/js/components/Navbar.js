957f17e51d20c05a8e8e7fbe57e6e17b85af2dfb
Αριστείδης Λαμπρινίδης <91885928+zeromaxx@users.noreply.github.com>
2023-12-01 11:23:14 +0200

    Update Navbar.js

--- resources/js/components/Navbar.js
+++ resources/js/components/Navbar.js
@@ -6,26 +6,25 @@ function NavBar() {
     return (
         <div className="header">
             <div className="container">
-                <div className="row">
+                <div className="row align-items-center">
                     <div className="col-4">
                         <div className="logo">
                             <img src={LogoImage} alt="" />
                         </div>
                     </div>
                     <div className="col-4">
-                        <ul className="nav-menu">
-                            <Link to="/">Home</Link>
-                            <Link to="/about">Fleet</Link>
+                        <ul className="nav-menu p-0 m-0">
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li className="ms-3">
+                                <Link to="/login">Login</Link>
+                            </li>
                         </ul>
                     </div>
                     <div className="col-4">
                         <div className="search-and-user">
-                            <input type="search" placeholder="Search" />
-                            <a href="#">Service & Contact</a>
-                            <Link to="/login">Login</Link>
-                            <select name="languages" id="language-select">
-                                <option value="en">EN</option>
-                            </select>
+                            <input className="form-control" type="search" placeholder="Search" />
                         </div>
                     </div>
                 </div>
