b518af541882addf57d689e16b09a1f5506dd61c
Αριστείδης Λαμπρινίδης <91885928+zeromaxx@users.noreply.github.com>
2023-12-01 16:35:02 +0200

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import Home from "./components/Home";
 import Login from "./components/Login";
+import Register from "./components/Register";
 import NotFoundPage from './components/NotFoundPage'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/login" element={<Login/>} />
+            <Route path="/register" element={<Register/>} />
             <Route path="*" element={<NotFoundPage />} />
         </Routes>
     );
