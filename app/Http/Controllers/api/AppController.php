035a3d3035cca301811dc3c993cb9a28a4128286
Αριστείδης Λαμπρινίδης <91885928+zeromaxx@users.noreply.github.com>
2023-12-04 15:03:22 +0200

    Update AppController.php

--- app/Http/Controllers/api/AppController.php
+++ app/Http/Controllers/api/AppController.php
@@ -1,9 +1,12 @@
 <?php
 
 namespace App\Http\Controllers\api;
-use App\Http\Controllers\Controller;
 use App\Models\Car;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AppController extends Controller
 {
@@ -17,5 +20,40 @@ class AppController extends Controller
     {
         $car = Car::where('id', $id)->first();
         return response()->json($car);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|max:255|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation errors',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        try {
+            $user = User::create([
+                'username' => $request->username,
+                'password' => Hash::make($request->password),
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'User successfully registered',
+            ]);
+        } catch (\Exception $e) {
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Registration failed',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 }
