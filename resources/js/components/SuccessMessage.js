59f26c25c9be2bd0a05b959d33247c49f469de35
Αριστείδης Λαμπρινίδης <91885928+zeromaxx@users.noreply.github.com>
2023-12-04 15:14:22 +0200

    Update SuccessMessage.js

--- resources/js/components/SuccessMessage.js
+++ resources/js/components/SuccessMessage.js
@@ -1,1 +1,11 @@
+import React from 'react';
 
+function SuccessMessage({ message }) {
+    return (
+        <div className="alert alert-success" role="alert">
+            {message}
+        </div>
+    );
+}
+
+export default SuccessMessage;
