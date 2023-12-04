e30252863f2fc97832d87b655a1ba2a883733182
Αριστείδης Λαμπρινίδης <91885928+zeromaxx@users.noreply.github.com>
2023-12-04 15:01:57 +0200

    Update Register.js

--- resources/js/components/Register.js
+++ resources/js/components/Register.js
@@ -1,5 +1,31 @@
+import React, { useState } from 'react';
 import LogoImage from "../../../public/img/LPALDlogoheader.webp";
+
 export default function Register() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        fetch('api/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    };
     return (
         <section>
             <div className="col-3">
@@ -7,12 +33,12 @@ export default function Register() {
                     <div className="text-center">
                         <img src={LogoImage} alt="Logo" className="mb-4 w-100" />
                         <h2 className="mb-4">Register Account</h2>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <div className="form-group mb-3">
-                                <input type="text" className="form-control" placeholder="Your Username" />
+                                <input type="text" className="form-control" placeholder="Your Username" value={username} onChange={(e) => setUsername(e.target.value)} />
                             </div>
                             <div className="form-group mb-3">
-                                <input type="password" className="form-control" placeholder="Your Password" />
+                                <input type="password" className="form-control" placeholder="Your Password"  value={password} onChange={(e) => setPassword(e.target.value)}/>
                             </div>
                             <button type="submit" className="btn btn-default">Register</button>
                         </form>
