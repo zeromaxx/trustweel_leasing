757dd2b8e8c86397c61b6a0281b71d7462d73a45
Αριστείδης Λαμπρινίδης <91885928+zeromaxx@users.noreply.github.com>
2023-12-04 15:14:53 +0200

    Update Register.js

--- resources/js/components/Register.js
+++ resources/js/components/Register.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import LogoImage from "../../../public/img/LPALDlogoheader.webp";
+import SuccessMessage from './SuccessMessage';
 
 export default function Register() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [success, setSuccess] = useState(null);
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -21,6 +23,11 @@ export default function Register() {
             .then(response => response.json())
             .then(data => {
                 console.log('Success:', data);
+                if (data.success) {
+                    setSuccess(data.message); // Set success message
+                } else {
+                    // Handle errors
+                }
             })
             .catch((error) => {
                 console.error('Error:', error);
@@ -38,10 +45,11 @@ export default function Register() {
                                 <input type="text" className="form-control" placeholder="Your Username" value={username} onChange={(e) => setUsername(e.target.value)} />
                             </div>
                             <div className="form-group mb-3">
-                                <input type="password" className="form-control" placeholder="Your Password"  value={password} onChange={(e) => setPassword(e.target.value)}/>
+                                <input type="password" className="form-control" placeholder="Your Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                             </div>
                             <button type="submit" className="btn btn-default">Register</button>
                         </form>
+                        {success && <SuccessMessage message={success} />}
                     </div>
                 </div>
             </div>
