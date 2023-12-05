190a9f7044fd6c69cea5bd4911779b2288c33946
Αριστείδης Λαμπρινίδης <91885928+zeromaxx@users.noreply.github.com>
2023-12-05 16:34:36 +0200

    Update Contact.js

--- resources/js/components/Contact.js
+++ resources/js/components/Contact.js
@@ -2,50 +2,49 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div> <div class="container mt-5">
-    <div class="row">
-      <div class="col-md-6 offset-md-3">
+    <div> <div className="container mt-5">
+    <div className="row">
+      <div className="col-md-6 offset-md-3">
         <h2>Contact details</h2>
         <p>Fill in the fields below to receive a quote.</p>
         <form>
-          <div class="mb-3">
-            <div class="row">
-              <div class="col">
-                <input type="text" class="form-control" placeholder="First name*" required>
+          <div className="mb-3">
+            <div className="row">
+              <div className="col">
+                <input type="text" className="form-control" placeholder="First name*" />
               </div>
-              <div class="col">
-                <input type="text" class="form-control" placeholder="Last name*" required>
+              <div className="col">
+                <input type="text" className="form-control" placeholder="Last name*" />
               </div>
             </div>
           </div>
-          <div class="mb-3">
-            <input type="email" class="form-control" placeholder="What is your e-mail?*" required>
+          <div className="mb-3">
+            <input type="email" className="form-control" placeholder="What is your e-mail?*" />
           </div>
-          <div class="mb-3">
-            <div class="input-group">
-              <span class="input-group-text" id="basic-addon1">📞</span>
-              <input type="tel" class="form-control" placeholder="Telephone*" aria-label="Telephone" aria-describedby="basic-addon1" required>
+          <div className="mb-3">
+            <div className="input-group">
+              <span className="input-group-text" id="basic-addon1">📞</span>
+              <input type="tel" className="form-control" placeholder="Telephone*" aria-label="Telephone" aria-describedby="basic-addon1" />
             </div>
           </div>
 
           <h2>Additional details</h2>
-          <div class="mb-3">
-            <select class="form-select" required>
+          <div className="mb-3">
+            <select className="form-select" required>
               <option value="">Please select</option>
-              <!-- Other options here -->
             </select>
           </div>
-          <div class="mb-3">
-            <input type="text" class="form-control" placeholder="Company name*" required>
+          <div className="mb-3">
+            <input type="text" className="form-control" placeholder="Company name*" />
           </div>
-          <div class="mb-3">
-            <input type="text" class="form-control" placeholder="VAT number*" required>
+          <div className="mb-3">
+            <input type="text" className="form-control" placeholder="VAT number*" />
           </div>
-          <div class="mb-3">
-            <input type="text" class="form-control" placeholder="City*" required>
+          <div className="mb-3">
+            <input type="text" className="form-control" placeholder="City*" />
           </div>
 
-          <button type="submit" class="btn btn-primary">Submit</button>
+          <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
     </div>
