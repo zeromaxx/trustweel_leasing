ae62646e0c831ab20ec2b999b59eccc1ab754251
Αριστείδης Λαμπρινίδης <91885928+zeromaxx@users.noreply.github.com>
2023-12-08 12:20:35 +0200

    Update AuthContext.js

--- resources/js/components/AuthContext.js
+++ resources/js/components/AuthContext.js
@@ -1,1 +1,22 @@
+import React, { useState, createContext } from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const logIn = () => {
+        setIsLoggedIn(true);
+    };
+
+    const logOut = () => {
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, logIn, logOut }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
 
