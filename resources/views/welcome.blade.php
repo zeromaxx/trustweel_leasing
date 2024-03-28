559906dea845e2f3a1bd86ee9db48b740fd2c43c
arislamp <129835271+arislamp@users.noreply.github.com>
2024-03-28 11:07:51 +0200

    u

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
     {{-- production --}}
-    {{-- <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}"> --}}
+    <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
     {{-- local --}}
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
